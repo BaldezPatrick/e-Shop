24ae172684b2e6a07c696a8544f66edf8f520f64
BaldezPatrick <patrickbaldex@gmail.com>
2022-12-19 19:19:17 -0300

    feat(header): First component: a horizontal nav bar.

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import './header.css'
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import StoreIcon from '@mui/icons-material/Store';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Header = () => {
+    return (
+        <>
+            <main className="header">
+                <section className="header_logo">
+
+                    <StoreIcon className="header_logo_img" fontSize='large' />
+                    <h2 className="header_logo_title">Shop</h2>
+
+                </section>
+
+                <section className="header_search">
+
+                    <input type="text" className="header_search_input" />
+                    <SearchIcon className="header_search_icon" />
+
+                </section>
+
+                <section className="header_nav">
+
+                    <section className="nav_item">
+
+                        <span className="nav_item_one">Hello Guest</span>
+                        <span className="nav_item_2">Sign In</span>
+
+                    </section>
+
+                    <section className="nav_item">
+
+                        <span className="nav_item_one">Your</span>
+                        <span className="nav_item_2">Shop</span>
+
+                    </section>
+
+                    <section className="nav_item">
+
+                        <ShoppingBasketIcon className="item_basket"/>
+                        <span className="nav_item_2 nav_basket_count">0</span>
+
+                    </section>
+
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default Header;
