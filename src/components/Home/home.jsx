3a6d9b1cc4362ce3def0949a6f4d00a8b01aa088
BaldezPatrick <patrickbaldex@gmail.com>
2022-12-19 22:35:11 -0300

    feat(Home): Add data to product props.

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -11,8 +11,13 @@ const Home = () => {
                     <img src="https://images.pexels.com/photos/5632371/pexels-photo-5632371.jpeg?auto=compress&cs=tinysrgb&w=400" alt="a supermarket trolley" className="home_img" />
 
                     <div className="home_row">
-                        <Product />
-                        <Product />
+                        <Product
+                        id={123}
+                        title={'Teste'}
+                        price={300}
+                        rating={3}
+                        image="https://images.pexels.com/photos/2905238/pexels-photo-2905238.jpeg?auto=compress&cs=tinysrgb&w=400"
+                        />
                     </div>
 
                     <div className="home_row">
