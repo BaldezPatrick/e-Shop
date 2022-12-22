fc83b1c21ab6568ea1f0b13a08bbc4a7b621da46
BaldezPatrick <patrickbaldex@gmail.com>
2022-12-21 22:52:37 -0300

    Add StateProvider in routes.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/routes';
 import './index.css';
+import reducer, { initialState } from './reducer';
+import { StateProvider } from './StateProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <RouterProvider router={router} />
+    </StateProvider>
   </React.StrictMode>
 );
