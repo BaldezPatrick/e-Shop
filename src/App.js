5b911059680464e3f9ebb7a4f4178fa66cde65d0
BaldezPatrick <patrickbaldex@gmail.com>
2022-12-24 14:13:48 -0300

    Rebuilding the project.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Header from './components/Header/header';
-import Home from './components/Home/home';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home />
+      <h1>OLÁ</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import router from './routes/routes';
-import './index.css';
-import reducer, { initialState } from './reducer';
-import { StateProvider } from './StateProvider';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <RouterProvider router={router} />
-    </StateProvider>
+    <App />
   </React.StrictMode>
 );
