5e5a68aec55b2720713fd66ccd071caf17ba57a9
BaldezPatrick <patrickbaldex@gmail.com>
2022-12-24 18:16:44 -0300

    New place for routes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import './App.css';
-import { RouterProvider } from 'react-router-dom';
-import routers from "./Routes/Routes";
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+} from "./Pages";
 
 function App() {
   return (
     <React.StrictMode>
-      <RouterProvider router={routers} />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<SingleProduct/>} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </React.StrictMode>
   );
 }
